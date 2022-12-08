adfeafb0e0054815c6262001f3752ce4efeb58dd
HansJoachimW <hansjw123@gmail.com>
2022-12-08 13:13:17 +0700

    model

---
+++ app/Http/Controllers/SubdistributorController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Subdistributor;
+use App\Http\Requests\StoreSubdistributorRequest;
+use App\Http\Requests\UpdateSubdistributorRequest;
+
+class SubdistributorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreSubdistributorRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreSubdistributorRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Subdistributor  $subdistributor
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Subdistributor $subdistributor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Subdistributor  $subdistributor
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Subdistributor $subdistributor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateSubdistributorRequest  $request
+     * @param  \App\Models\Subdistributor  $subdistributor
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateSubdistributorRequest $request, Subdistributor $subdistributor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Subdistributor  $subdistributor
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Subdistributor $subdistributor)
+    {
+        //
+    }
+}
