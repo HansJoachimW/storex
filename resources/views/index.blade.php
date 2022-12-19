bade006ee4e508d524974dc717e15a7170e2d369
kennethraff <kraffelino@student.ciputra.ac.id>
2022-12-19 12:46:49 +0700

    Update index.blade.php
    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,26 @@
 @extends('layouts.main')
 
 @section('container')
+    <meta name="description" content="Bootstrap 4 Carousel - Random Slide" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+    {{-- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+</script> --}}
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
+
     <!-- Navbar-->
     <nav class="navbar navbar-expand-lg ">
         <div class="container">
-            <a class="navbar-brand" href="#"><img src="https://images.tokopedia.net/img/cache/215-square/GAnVPX/2021/11/24/968e9cdc-8dc0-40be-96bd-649ac23b2424.png" alt="" width="50px" height=""></a>
-            
+            <a class="navbar-brand" href="#"><img
+                    src="https://images.tokopedia.net/img/cache/215-square/GAnVPX/2021/11/24/968e9cdc-8dc0-40be-96bd-649ac23b2424.png"
+                    alt="" width="50px" height=""></a>
+
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
                 aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -67,15 +82,50 @@
         </div>
         <!-- Akhir Panel-->
 
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100 rounded"
+                        src="https://i.vgy.me/3WfPV1.png"
+                        alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100 rounded"
+                        src="https://i.vgy.me/WVhkMq.png"
+                        alt="Second slide">
+                </div>
+                <div class="carousel-item rounded">
+                    <img class="d-block w-100"
+                        src="https://i.vgy.me/pI0Y7d.png"
+                        alt="Third slide">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+
         <!--Welcome-->
         <div class="row welcome mb-3">
             <div class="col-sm">
-                <img src="../img/pic1.jpg" alt="" class="img-fluid rounded ukGambar justify-content-center rounded">
+                <img src="../img/pic1.jpg" alt=""
+                    class="img-fluid rounded ukGambar justify-content-center rounded">
             </div>
             <div class="col">
                 <h3>Apa yang <br> <span>StoreX Indonesia</span><br> Jual?</h3>
                 <div class="container mt-2 justify-content-center">
-                    <p>Pusatnya produk IT dan Elektronik Surabaya. Dijamin akan pelayanan serta kecepatan pengiriman <br> Seluruh paket harap video unboxing menunjukkan resi+ seluruh bagian paket hingga produk   </p>
+                    <p>Pusatnya produk IT dan Elektronik Surabaya. Dijamin akan pelayanan serta kecepatan pengiriman <br>
+                        Seluruh paket harap video unboxing menunjukkan resi+ seluruh bagian paket hingga produk </p>
                 </div>
 
                 <!-- <a href="" class="btn btn-primary">KLIK</a> -->
@@ -104,5 +154,4 @@
             </div>
         </div>
     </div>
-    <!-- Akhir Container-->
 @endsection
